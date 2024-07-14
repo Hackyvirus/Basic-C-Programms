94226231df53badd437d5c253adb6b4f9402a38a
sushantatram <hackyvirus591081@gmail.com>
2024-07-14 21:48:39 +0530

    13th programm completed

--- 13.c
+++ 13.c
@@ -2,15 +2,16 @@
 // volume of a cylinder given its radius and height
 
 #include <stdio.h>
-int main() {
-  float rad, areaOfCircle, valumeOfCylinder, height;
-  printf("Enter the radius of circle: ");
-  scanf("%f", &rad);
-  areaOfCircle = 3.14 * rad * rad;
-  printf("Enter the Height of Cylinder: ");
-  scanf("%f", &height);
-  valumeOfCylinder = areaOfCircle * height;
-  printf("Area of Circle: %.2f\n", areaOfCircle);
-  printf("Volume of Cylinder: %.2f\n", valumeOfCylinder);
-  return 0;
+int main()
+{
+    float rad, areaOfCircle, valumeOfCylinder, height;
+    printf("Enter the radius of circle: ");
+    scanf("%f", &rad);
+    areaOfCircle = 3.14 * rad * rad;
+    printf("Enter the Height of Cylinder: ");
+    scanf("%f", &height);
+    valumeOfCylinder = areaOfCircle * height;
+    printf("Area of Circle: %.2f\n", areaOfCircle);
+    printf("Volume of Cylinder: %.2f\n", valumeOfCylinder);
+    return 0;
 }
